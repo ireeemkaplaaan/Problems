13796366e681248f4c09bdbc9b6abba3eca0c131
ireeemkaplaaan <kaplanrem@gmail.com>
2020-06-19 18:27:43 +0300

    Create Problem2.cs

---
+++ Problem2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("a sayısını gir: ");
+		int a=Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("b sayısını gir: ");
+		int b=Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("x sayısını gir: ");
+		int x=Convert.ToInt32(Console.ReadLine());
+		toplama(a,b,x);
+		
+	}
+	static void toplama(int a, int b, int x)
+	{int toplam=0;
+		for(int i=a;i<b;i++)
+		{
+			if(i%x==0)
+				toplam=toplam+i;
+		}
+	 Console.WriteLine(toplam);
+	}
+}
